12f4a585a8c53b1211eb4816c6b79b7a71a4341b
WhoseBugApple <792715299@qq.com>
2024-04-24 14:26:27 +0800

    KORC BACKUP: 2024-04-24 14:26:25

--- copyAsMarkdownLink.js
+++ copyAsMarkdownLink.js
@@ -68,6 +68,8 @@ async function main() {
 
         function removeGarbageCharacter(text) {
             if (text == undefined) return "";
+            // remove link symbol emoji the char of unicode codepoint 128279 0x1F517 UTF16 BE D83D DD17
+            text = text.replace(/\uD83D\uDD17/g, '');
             // remove emoji
             text = text.replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, ' ? ');
             // remove Zero-Width Char
